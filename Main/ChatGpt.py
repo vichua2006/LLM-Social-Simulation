fa8c3bb27b392290e7caae3c6f0496d83b4598ff
li08jhan <143908151+li08jhan@users.noreply.github.com>
2023-09-11 16:28:01 -0400

    Update ChatGpt.py

--- Main/ChatGpt.py
+++ Main/ChatGpt.py
@@ -6,8 +6,9 @@ import openai  # OpenAI for interacting with the GPT-3 model
 # Setting the OpenAI API key
 cheops_key='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'# for testing purposes
 charles_key="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"
+Jinhan_key = "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"
 
-os.environ["OPENAI"] = charles_key
+os.environ["OPENAI"] = Jinhan_key
 
 openai.api_key = os.environ["OPENAI"]
 # Function to interact with the GPT-3 model and get response
