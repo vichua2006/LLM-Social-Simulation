b911a5ae80453ff8f61fee7d1f009be3e3392e2f
CyborSphinx <102343276+CyborSphinx@users.noreply.github.com>
2023-09-10 23:25:50 -0400

    Clean up

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def create_individual_layout(individual: List[Individual]) -> sg.TabGroup:
 
 
 def start_simulate(system:System):
-    for i in range(25):
+    for i in range(50):
         print(f"DAY {system.time+1} HAS STARTED.")
         simulate(system.individuals, system)
 def main():
