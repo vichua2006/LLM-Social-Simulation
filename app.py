aaa442f90b9f046d9e495b8324058888bcb71c78
CyborSphinx <102343276+CyborSphinx@users.noreply.github.com>
2023-09-10 23:54:56 -0400

    50 Days While loop

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def create_individual_layout(individual: List[Individual]) -> sg.TabGroup:
 
 
 def start_simulate(system:System):
-    for i in range(50):
+    while system.time<50:
         print(f"DAY {system.time+1} HAS STARTED.")
         simulate(system.individuals, system)
 def main():
