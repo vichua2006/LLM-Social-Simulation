88a940b6322dfd8d18eea20998bb631f0ff6baf4
li08jhan <143908151+li08jhan@users.noreply.github.com>
2024-01-14 23:55:08 +0800

    Update
    replace the memory with the memorystream and concept nodes system(from stanford project)

--- test_memory.py
+++ test_memory.py
@@ -1,15 +1,18 @@
+import Main.Memory
+
 from Main.Individual import Individual
 from Main.Memory import ConceptNode
-import Retrieve
 
-Person = Individual(0,"a")
+person = Individual(0,"a")
 
 rob_memory = ConceptNode(1,"rob",1,0,"rob",[1],10,"person 0 rob person 1", 6)
-farm_memory = ConceptNode(2,"farm",2, 0, "farm", [], 10, "person 0 farms and receives 10 units of food", 3)
-chat_memory = ConceptNode(3, "chat", 2, 0, "chat", [1,2], 0, "person 0 said \'Good Morning\' to person 1,2", 5)
+#farm_memory = ConceptNode(2,"farm",2, 0, "farm", [], 10, "person 0 farms and receives 10 units of food", 3)
+#chat_memory = ConceptNode(3, "chat", 2, 0, "chat", [1,2], 0, "person 0 said \'Good Morning\' to person 1,2", 5)
 
-Person.memorystream.add_concept_node(rob_memory)
-Person.memorystream.add_concept_node(rob_memory)
-Person.memorystream.add_concept_node(rob_memory)
+person.memorystream.add_concept_node(rob_memory)
+#person.memorystream.add_concept_node(rob_memory)
+#person.memorystream.add_concept_node(rob_memory)
 
-print(Person.memorystream)
+#print(person.memorystream)
+
+print(person.memorystream.emotion)
