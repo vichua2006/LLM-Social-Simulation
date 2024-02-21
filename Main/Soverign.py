7b3a9cb77394f6f2a3ed0ae4193cd1ff51d7beea
CyborSphinx <102343276+CyborSphinx@users.noreply.github.com>
2024-02-20 22:14:05 -0500

    Update Soverign.py

--- Main/Soverign.py
+++ Main/Soverign.py
@@ -1,7 +1,7 @@
-interval_of_properity=["poor","medium income","affluent"]# to be swapped with two touple integer value.
+interval_of_properity=["poor","medium income","affluent"]# DATA: to be swapped with integer two touple values.
 society_description=None #Describe the society and the things going on. The mechanisms of each actions. how much one consumes, how much one produce. How much people. General context can include the contextual prompts we’ve written before, just adding the sovereign and other parts.
 equality_purpose_statement="You are a wise king who cares for the people, and you want to promote equity in your realm of control so that it can be a just society where each person leads a life of equality and respect."
-food_std,food_mean,land_std,land_mean=None#WIP
+food_std,food_mean,land_std,land_mean=None#data
 fixed_context=f'''You are the king of the society. {society_description}
 {equality_purpose_statement}
 Society's food Mean {"[{},{})".format(interval_of_properity[0][0],interval_of_properity[0][1])}: poor
@@ -14,20 +14,20 @@ Without land, individual cannot farm and produce food to survive.
 If someone's land ownership is below {land_mean}(mean) - 2*{land_std}(standard deviation)={land_mean-2*land_std}, then this person is relatively poor, and might be ashamed of it.
 If someone's land ownership is above {land_mean}(mean) + 2*{land_std}(standard deviation)={land_mean+2*land_std}, then this person is relatively wealthy and might be proud of it.
 '''
-individual_wealth={} # dict{name:wealth}, make it sorted, descending order.
-enumerate_wealth=""#WIP
+individual_wealth={} #DATA: dict{name:wealth}, make it sorted, descending order.
+enumerate_wealth=""
 for i in individual_wealth:enumerate_wealth+='{} has {} in farmer currency.'.format(i,individual_wealth[i])
 gdp=sum(individual_wealth.items())
-gini=None#WIP
-person_change_in_wealth={}#WIP
+gini=None#DATA
+person_change_in_wealth={}#DATA
 enumerate_change=""
 for i in person_change_in_wealth:
     changed="dropped" if person_change_in_wealth[i]<0 else "increased"
     enumerate_change+='{}\'s wealth has {} by {}.'.format(i,changed,person_change_in_wealth[i])
-dGDP=None#WIP
-mean_production=None#WIP
-rate_of_activities=""#WIP
-goods_distribution=""#WIP
+dGDP=None#DATA
+mean_production=None#DATA
+rate_of_activities=""#DATA
+goods_distribution=""#DATA
 conversations=""#WIP
 live_data=f'''LIVE DATA: The individuals wealth listed below in descending order along with their change in wealth:{enumerate_wealth, enumerate_change }
 
